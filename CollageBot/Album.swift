50883884f622adaf9df3dbad9cee7a2617eb2988
Haaris Muneer <haarismuneer@gmail.com>
2018-11-01 14:16:46 -0400

    fix incorrect album properties

--- CollageBot/Album.swift
+++ CollageBot/Album.swift
@@ -7,16 +7,25 @@ import Foundation
 
 struct Album {
     
-    var title: String!
-    var artistName: String!
-    var playCount: String!
-    var imageURL: URL!
+    var title: String?
+    var artistName: String?
+    var playCount: String?
+    var imageURL: URL?
     
     init(dictionary: [String: Any]) {
         title = dictionary["name"] as? String
-        artistName = dictionary["name"] as? String
-        playCount = dictionary["name"] as? String
-        imageURL = URL(string: dictionary["name"] as? String ?? "") // replace with default image link
+        artistName = getArtistName(dictionary["artist"] as? [String: Any])
+        playCount = dictionary["playcount"] as? String
+        imageURL = URL(string: getImageURL(dictionary["image"] as? [[String: String]])) // replace with default image link
+    }
+    
+    private func getArtistName(_ dict: [String: Any]?) -> String {
+        return dict?["name"] as? String ?? ""
+    }
+    
+    private func getImageURL(_ images: [[String: String]]?) -> String {
+        let largestImageDictionary = images?.filter { return $0["size"] == "extralarge" }.first
+        return largestImageDictionary?["#text"] ?? ""
     }
     
 }
